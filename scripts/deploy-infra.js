98820fca4dabe685c8b80074abbcb6358ae2e5df
grkmyvz <gorkem.yavuz.39@hotmail.com>
2024-06-21 03:18:41 +0300

    Added deploy script for infra

---
+++ scripts/deploy-infra.js
@@ -0,0 +1,55 @@
+const { exec } = require('child_process');
+const hre = require('hardhat');
+
+function runVerifyContractCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+      } else {
+        console.log(stdout);
+        resolve(true);
+      }
+    });
+  });
+}
+
+async function main() {
+  // Deploy the contract
+  console.log('Deploying infrastructure contracts...');
+
+  const oracleAddress = network.name === 'optimisim' ? '0x0708325268dF9F66270F1401206434524814508b' : '0xA2aa501b19aff244D90cc15a4Cf739D2725B5729';
+  const pythPriceFeeds = await hre.ethers.deployContract('PythPriceFeeds', [oracleAddress]);
+  await pythPriceFeeds.waitForDeployment();
+
+  const routerAddress = network.name === 'optimism' ? '0x0000000000000000000000000000000000000000' : '0x8cfb0faEA320A9Dfa41C1eF31eea88A50D1F020e';
+  const uniswapV2 = await hre.ethers.deployContract('UniswapV2', [routerAddress]);
+  await uniswapV2.waitForDeployment();
+
+  console.log(`PythPriceFeeds deployed to ${pythPriceFeeds.target}`);
+  console.log(`UniswapV2 deployed to ${uniswapV2.target}`);
+
+  // Verify the contracts
+  console.log('Verifying contracts...');
+  const ppfCommand = `npx hardhat verify --network ${network.name} ${pythPriceFeeds.target} ${oracleAddress}`;
+  try {
+    await runVerifyContractCommand(ppfCommand);
+  } catch (error) {
+    console.error('❌ Error verifying contract: ', error);
+  }
+
+  const uniswapCommand = `npx hardhat verify --network ${network.name} ${uniswapV2.target} ${routerAddress}`;
+  try {
+    await runVerifyContractCommand(uniswapCommand);
+  } catch (error) {
+    console.error('❌ Error verifying contract: ', error);
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
